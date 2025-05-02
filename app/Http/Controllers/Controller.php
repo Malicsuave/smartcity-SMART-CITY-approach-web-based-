73d45739783b82923678fb3c17abcd675bf96416
ayawnamagcodenidj <aquinodeej@gmail.com>
2025-05-02 14:42:43 +0800

    sample

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+
 
 
 class Controller extends BaseController
